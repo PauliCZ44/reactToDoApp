4e751701f8097f3bd857320b4964ae68bbb37eda
PauliCZ44 <stastnyp@seznam.cz>
2020-07-31 20:15:08 +0200

    Updates

---
+++ precache-manifest.941dbfc5a871337dab69674360e17a07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfcbc4e5dce3f8451a3c024a675d3bbc",
+    "url": "/reactToDoApp/index.html"
+  },
+  {
+    "revision": "1d20cca5be8fe8652a53",
+    "url": "/reactToDoApp/static/css/main.cfbb1734.chunk.css"
+  },
+  {
+    "revision": "5cc89670cd000ec30016",
+    "url": "/reactToDoApp/static/js/2.e190c17f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDoApp/static/js/2.e190c17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d20cca5be8fe8652a53",
+    "url": "/reactToDoApp/static/js/main.da94104b.chunk.js"
+  },
+  {
+    "revision": "1b075b7364cf002957e2",
+    "url": "/reactToDoApp/static/js/runtime-main.3307aba2.js"
+  }
+]);
